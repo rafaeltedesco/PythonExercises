adfe67ae3132e7b82fde835f1f0f5f203850114f
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-02-23 19:49:25 -0300

    exercise: evens_between_two_numbers

---
+++ README.md
@@ -0,0 +1,5 @@
+# Repositório de Exercícios para meus Alunos de Programação Python
+
+- [evens_between_two_numbers]('https://github.com/rafaeltedesco/PythonExercises/evens_between_two_numbers)
+
+> Imprimir a lista de números pares e quantos existem entre dois números informados pelo usuário
---
+++ evens_between_two_numbers/evens.py
@@ -0,0 +1,15 @@
+# Desafio: Obter a lista e a contagem de números pares entre dois números informados pelo usuário
+
+def get_even_numbers(a, b):
+    if a == b:
+        return []
+    first_number, last_number = (a, b) if a < b else (b, a)
+    evens = []
+    [evens.append(i)
+     for i in range(first_number + 1, last_number) if i % 2 == 0]
+    return {'evens': evens, 'count': len(evens)}
+
+
+a, b = list(
+    map(int, input("Digite dois números separados por espaço: ").split(" ")))
+print(get_even_numbers(a, b))
