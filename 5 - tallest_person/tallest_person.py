661dc8745834968d8e6363c55bafda7bab4e7512
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2022-03-05 00:16:40 -0300

    add: new exercise

--- 5 - tallest_person/tallest_person.py
+++ 5 - tallest_person/tallest_person.py
@@ -16,10 +16,3 @@ def get_average_height_by_sex(sex: str, list_of_persons: List[Dict[str, float]])
                 'height': person['height'] + next_person['height']},
             filter(lambda person: person['sex'] == sex, list_of_persons), {'height': 0})['height'], 2)
     return {'avg_height': round(avg_height / len(list(filter(lambda person: person['sex'] == sex, list_of_persons))), 2)}
-
-
-def generate_random_height(list_of_heights: List[float]):
-    new_height = round(random.random() + 1, 2)
-    if new_height in list_of_heights:
-        generate_random_height(list_of_heights)
-    return [*list_of_heights, new_height]
